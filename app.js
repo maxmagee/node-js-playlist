55f4db1ee743b9933b6b7ac0fa9edf4d6dfd6020
Max Magee <maxmagee@users.noreply.github.com>
2016-07-17 00:04:45 -0500

    Event emmitter lesson 8.

--- app.js
+++ app.js
@@ -1,4 +1,23 @@
-var stuff = require('./stuff');
+var events = require('events');
+var util = require('util');
 
-console.log(stuff.counter(['max', 'adam', 'ben']));
-console.log(stuff.pi);
+var Person = function(name) {
+  this.name = name;
+};
+
+util.inherits(Person, events.EventEmitter);
+
+var james = new Person('james');
+var mary = new Person('mary');
+var bob = new Person('bob');
+
+var people = [james, mary, bob];
+
+people.forEach(function(person) {
+  person.on('speak', function(message) {
+    console.log(person.name + ' said: ' + message);
+  });
+});
+
+james.emit('speak', 'hey dudes');
+mary.emit('speak', 'hi James!');
