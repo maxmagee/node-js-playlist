0ddb759d27e41e7435a29e5c60d2d43eebeafc56
Max Magee <maxmagee@users.noreply.github.com>
2016-07-17 21:59:45 -0500

    Serving up static HTML page lesson 17

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
 var http = require('http');
+var fs = require('fs');
 
 var server = http.createServer(function(req, resp) {
+  var myReadStream = fs.createReadStream(__dirname + '/index.html', 'utf8');
   console.log('request was made: ' + req.url);
-  resp.writeHead(200, {'Content-Type': 'text/plain'});
-  resp.end('Hey ninjas');
+
+  resp.writeHead(200, {'Content-Type': 'text/html'});
+  myReadStream.pipe(resp);
 });
 
 server.listen(3000, '127.0.0.1');
